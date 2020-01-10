33b034a320e090e150a26c9b607a3e7b065b4202
Tim Boring <tim@boring.green>
2020-01-10 17:27:53 -0500

    update docstrings and mark several methods protected

--- src/AtlasI2C.py
+++ src/AtlasI2C.py
@@ -1,7 +1,16 @@
-"""Class to communicate with Atlas Scientific I2C sensors.
+"""Class to communicate with Atlas Scientific EZO sensors in I2C mode.
 
 Source code is based on examples from Atlas Scientific:
 https://github.com/AtlasScientific/Raspberry-Pi-sample-code/blob/master/AtlasI2C.py
+
+Relevant datasheets for each of the EZO sensors:
+pH: https://www.atlas-scientific.com/_files/_datasheets/_circuit/pH_EZO_Datasheet.pdf
+temp: https://www.atlas-scientific.com/_files/_datasheets/_circuit/EZO_RTD_Datasheet.pdf
+orp: https://www.atlas-scientific.com/_files/_datasheets/_circuit/ORP_EZO_datasheet.pdf
+do: https://www.atlas-scientific.com/_files/_datasheets/_circuit/DO_EZO_Datasheet.pdf
+ec: https://www.atlas-scientific.com/_files/_datasheets/_circuit/EC_EZO_Datasheet.pdf
+co2: https://www.atlas-scientific.com/_files/_datasheets/_probe/EZO_CO2_Datasheet.pdf
+flo: https://www.atlas-scientific.com/_files/_datasheets/_circuit/flow_EZO_Datasheet.pdf
 """
 
 import io
@@ -49,11 +58,11 @@ class AtlasI2C:
         self.address = addr
 
     def write(self, cmd: str) -> None:
-        """Append the null character and sends the string over I2C."""
+        """Append the null character and send the string over I2C."""
         cmd += "\00"
         self.device_file.write(cmd.encode("latin-1"))
 
-    def response_valid(self, response: bytes) -> bool:
+    def _response_valid(self, response: bytes) -> bool:
         valid: bool = True
         error_code: Optional[str] = None
         if len(response) > 0:
@@ -70,7 +79,7 @@ class AtlasI2C:
         """Read a specified number of bytes from I2C."""
 
         raw_data: bytes = self.device_file.read(num_of_bytes)
-        is_valid: bool = self.response_valid(response=raw_data)
+        is_valid: bool = self._response_valid(response=raw_data)
 
         if is_valid:
             data = raw_data[1:].strip().strip(b"\x00")
@@ -81,7 +90,7 @@ class AtlasI2C:
 
         return result
 
-    def get_command_timeout(self, command: str) -> Optional[float]:
+    def _get_command_timeout(self, command: str) -> Optional[float]:
         timeout: Optional[float] = None
         if command.upper().startswith(LONG_TIMEOUT_COMMANDS):
             timeout = self.long_timeout
@@ -91,9 +100,9 @@ class AtlasI2C:
         return timeout
 
     def query(self, command) -> Union[float, str]:
-        """Write a command to the board and read the response."""
+        """Write a command to the sensor and read the response."""
         self.write(command)
-        current_timeout: Optional[float] = self.get_command_timeout(command=command)
+        current_timeout: Optional[float] = self._get_command_timeout(command=command)
         if not current_timeout:
             return "sleep mode"
         else:
@@ -101,8 +110,7 @@ class AtlasI2C:
             return self.read()
 
     def close(self):
-        self.file_read.close()
-        self.file_write.close()
+        self.device_file.close()
 
     def list_i2c_devices(self) -> List[int]:
         """List I2C devices.
